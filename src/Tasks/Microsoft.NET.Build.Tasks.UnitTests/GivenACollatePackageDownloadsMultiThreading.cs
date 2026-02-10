fa35c15a42be62ba66706bf2da1b4fce04eca7b3
Tomas Bartonek <tbartonek@microsoft.com>
2026-02-10 12:37:44 +0100

    Migrate CollatePackageDownloads to multithreading - attribute-only

---
+++ src/Tasks/Microsoft.NET.Build.Tasks.UnitTests/GivenACollatePackageDownloadsMultiThreading.cs
@@ -0,0 +1,16 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+using FluentAssertions;
+using Microsoft.Build.Framework;
+using Xunit;
+namespace Microsoft.NET.Build.Tasks.UnitTests
+{
+    public class GivenACollatePackageDownloadsMultiThreading
+    {
+        [Fact]
+        public void ItHasMSBuildMultiThreadableTaskAttribute()
+        {
+            typeof(CollatePackageDownloads).Should().BeDecoratedWith<MSBuildMultiThreadableTaskAttribute>();
+        }
+    }
+}
